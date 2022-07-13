8084afd400b1967389f7f15fbddd9a0e0e19094c
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-07-12 22:59:21 -0300

    feat: confing react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './themes/default'
-import { GlobalStyle } from './themes/global'
+import { Router } from './Router'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
